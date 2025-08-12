4d856770e8bb0f3da798fa5a621c10b94e6c0413
guohaoxiang <guohaoxiangxiang@gmail.com>
2025-08-12 19:11:01 +0800

    remove extra files

--- code/MoGe/batch.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-base_dir = "/mnt/workspace/zhongqi.yang/VideoInpainting_new/ViewCrafter/extern/MoGe/example_images/pano_examples"
-cases = os.listdir(base_dir)
-for c in cases:
-    os.system(f"python scripts/infer_panorama.py --input {os.path.join(base_dir, c)} --output /mnt/workspace/zhongqi.yang/VideoInpainting_new/ViewCrafter/extern/MoGe/outputs/pano/ --maps --glb --ply")
